1f6bf065f2f5c0d22bcbe7d107e29a3ebc389e2f
JosephEJohnston <1193347519@qq.com>
2023-01-24 16:36:12 +0800

    feat: main scene init

--- src/main/java/MainSceneController.java
+++ src/main/java/MainSceneController.java
@@ -1,8 +1,12 @@
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.TextField;
 
+import java.io.File;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 
 public class MainSceneController implements Initializable {
@@ -13,5 +17,22 @@ public class MainSceneController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
+    }
+
+    public void changeDirectory(ActionEvent event) {
+        String directoryPath = pathTextField.getText();
+
+        File directory = new File(directoryPath);
+        if (!directory.isDirectory()) {
+            return;
+        }
+
+        File[] files = directory.listFiles();
+        if (files == null) {
+            return;
+        }
+
+        List<File> fileList = new ArrayList<>(List.of(files));
+        fileList.forEach(System.out::println);
     }
 }
